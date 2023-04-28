5f27545c8093ef6fe2d99fc1ae4fc66f28ec93b9
H4T3H4K3RS <sevavov.2004@gmail.com>
2023-04-28 14:11:50 +0000

    Deploying to release from @ H4T3H4K3RS/tradeitgui@3a9810af364c18ad0caed4e509f5755876824fb3 🚀

--- assets/useArbsStore.f78719c3.js
+++
@@ -1,1 +0,0 @@
-import{I as t,J as r}from"./index.cd0f8983.js";const o=t("ArbsStore",{actions:{fetchCex(e){return r.get("/arbs/cex",{params:e})},fetchDex(e){return r.get("/arbs/dex",{params:e})}}});export{o as u};
--- assets/useDefaultStore.08104bb8.js
+++
@@ -1,1 +0,0 @@
-import{I as t,J as a}from"./index.cd0f8983.js";t({id:"DefaultStore",actions:{fetchRates(e){return a.get("/rates",{params:e})}}});
