04f4d0245fe0a70780d90530a6eef03ff05c0c84
Ovchinnikov Vsevolod <sevavov.2004@gmail.com>
2023-05-06 21:05:15 +0300

    Fixed axios instance

--- src/layouts/components/Footer.vue
+++ src/layouts/components/Footer.vue
@@ -3,7 +3,7 @@
     <!-- 👉 Footer: left content -->
     <span class="d-flex align-center">
       &copy;
-      {{ new Date().getFullYear() }}
+      {{ new Date ().getFullYear () }}
       Made With
       <VIcon
         icon="tabler-heart"
@@ -11,27 +11,28 @@
         size="1.25rem"
         class="mx-1"
       />
-      By <a
+      For <a
         href="https://vk.com/hydralabs"
         target="_blank"
         rel="noopener noreferrer"
         class="text-primary ms-1"
-      >HydraLabs</a>
+      >CS HSE</a>
     </span>
     <!-- 👉 Footer: right content -->
     <span class="d-md-flex gap-x-4 text-primary d-none">
       <a
-        href="lorem"
-        target="noopener noreferrer"
-      >Lorem</a>
+        href="https://t.me/pseudomention"
+        rel="noreferrer noopener"
+        target="_blank"
+      >Ovchinnikov</a>
       <a
-        href="lorem"
-        target="noopener noreferrer"
-      >Lorem</a>
-      <a
-        href="lorem"
-        target="noopener noreferrer"
-      >Lorem</a>
+        href="https://t.me/barkovbg"
+        rel="noreferrer noopener"
+        target="_blank"
+      >Barkov</a>
     </span>
   </div>
 </template>
+
+<script setup>
+</script>
--- src/services/api.js
+++ src/services/api.js
@@ -1,10 +1,14 @@
 import axios from "axios"
 
 const instance = axios.create ({
-  baseURL: `${import.meta.env.VITE_API_URL}`,
-  headers: {
+  baseURL: `${import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : "http://localhost:8888/api/v1"}`,
+
+  // baseURL: "https://api.happynewyear.space/api/v1",
+  headers: import.meta.env.DEV ? {
     "Content-Type": "application/json",
     "ngrok-skip-browser-warning": "69420",
+  } : {
+    "Content-Type": "application/json",
   },
   validateStatus: function (status) {
     return true
