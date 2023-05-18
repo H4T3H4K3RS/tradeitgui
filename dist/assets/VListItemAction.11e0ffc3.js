7da5d56c7d3b71279c02d709d94b14ab63823bd7
H4T3H4K3RS <sevavov.2004@gmail.com>
2023-05-18 14:28:45 +0000

    Deploying to release from @ H4T3H4K3RS/tradeitgui@abada519c254699b49e34c9623c46a164c1bab59 🚀

---
+++ dist/assets/VListItemAction.11e0ffc3.js
@@ -0,0 +1,1 @@
+import{R as s,X as n,a6 as o,p as i}from"./index.2deb187d.js";const r=s({name:"VListItemAction",props:{start:Boolean,end:Boolean,...n()},setup(t,e){let{slots:a}=e;return o(()=>i(t.tag,{class:["v-list-item-action",{"v-list-item-action--start":t.start,"v-list-item-action--end":t.end}]},a)),{}}});export{r as V};
